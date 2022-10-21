321ed72e1237987b51f096224d1877d0b2ce6065
Felix Cagatin <cagatingilbert@gmail.com>
2022-10-20 19:22:41 -0700

    Created Reservation schema and model

---
+++ server/models/Reservation.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const reservationSchema = new Schema({
+    // Type of Service being reserved for
+    type: {
+        type: String,
+        required: True
+    },
+    // Comments made by the User
+    comments: [
+        {
+            commentText: {
+                type: String,
+                minlength: 1,
+                maxLength: 280
+            },
+        }
+    ],
+    // Price of the service
+    price: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        requried: true
+    },
+    slot: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'TimeSlot'
+        }
+    ]
+});
+
+const Reservation = model('Reservation', reservationSchema);
+module.exports = Reservation;

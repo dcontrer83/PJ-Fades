859abee63d4294856a080f10f993a367558aa67a
dcontrer83 <dcontrer83@yahoo.com>
2022-10-27 04:09:30 -0700

    Change ADD_PROFILE to ADD_USER and fix bug for addUser mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
-export const ADD_PROFILE = gql`
-mutation addUser($username: String!, $email: String!, $password: String) {
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
         token
         user {

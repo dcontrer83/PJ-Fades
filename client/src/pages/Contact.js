c574ef5aea9bc4a7ba293f4eeba192cd8e4f2f66
DanielK <Danielkang13@gmail.com>
2022-10-27 19:08:24 -0700

    added contact information

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -52,9 +52,8 @@ const Contact = () => {
             <div style={style.main}>
                 <div className='text-center' style={style.misc}>
                     <h1>Contact</h1>
-                    <p>Phone number: </p>
-                    <p>Email: </p>
-                    <p>Instagram: </p>
+                    <p className='fs-3'>Phone number: 1-951-553-4409</p>
+                    <p className='fs-3'>DM me on <a href="https://instagram.com/pushnpj?igshid=YmMyMTA2M2Y=">Instagram!</a></p>
                 </div>
                 <div className='bg-dark text-white mx-auto' style={style.formBox}>
                     <h2 className='text-center mb-0' style={style.sendTitle}>Send a Message!</h2>

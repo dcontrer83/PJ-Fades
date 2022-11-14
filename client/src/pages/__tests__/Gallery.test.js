849a37deb5f2a54ac8fe4a64c50ac44ba4647065
dcontrer83 <dcontrer83@yahoo.com>
2022-11-14 02:34:15 -0800

    Create pages Gallery test file. Create unit tests for div elements to test if it is in the document, specific style, and specific className.

---
+++ client/src/pages/__tests__/Gallery.test.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
+import pretty from 'pretty';
+import Gallery from '../Gallery';
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent, waitFor, getByTestId } from '@testing-library/react';
+
+globalThis.IS_REACT_ACT_ENVIRONMENT = true;
+
+let root;
+let container;
+
+beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    act(() => {
+        root = createRoot(container);
+    })
+});
+
+afterEach(() => {
+    act(() => {
+        root.unmount();
+    })
+    container.remove();
+    container = null;
+});
+
+let div1Element;
+let div2Element;
+let div3Element;
+let div4Element;
+let div5Element;
+
+function getAllDivElements() {
+    render(<Gallery />);
+    div1Element = screen.getByTestId('div-1-element');
+    div2Element = screen.getByTestId('div-2-element');
+    div3Element = screen.getByTestId('div-3-element');
+    div4Element = screen.getByTestId('div-4-element');
+    div5Element = screen.getByTestId('div-5-element');
+}
+
+describe('Div elements', () => {
+    it('should be in the document', () => {
+        getAllDivElements();
+        expect(div1Element).toBeInTheDocument();
+        expect(div2Element).toBeInTheDocument();
+        expect(div3Element).toBeInTheDocument();
+        expect(div4Element).toBeInTheDocument();
+        expect(div5Element).toBeInTheDocument();
+    });
+    it('should have specific style', () => {
+        getAllDivElements();
+        expect(div1Element).toHaveStyle('height: 75px');
+        expect(div2Element).toHaveStyle('height: 1000px');
+        expect(div3Element).toHaveStyle('height: 95%; width: 100%');
+        expect(div4Element).toHaveStyle('height: 50%; width: 100%');
+        expect(div4Element).not.toHaveStyle('height: 90%');
+        expect(div5Element).toHaveStyle('height: 50%; width: 100%');
+        expect(div5Element).not.toHaveStyle('height: 90%');
+    });
+    it('should have specific className', () => {
+        getAllDivElements();
+        expect(div3Element).toHaveClass('d-flex flex-column flex-lg-row align-items-center');
+        expect(div4Element).toHaveClass('bg-light d-flex justify-content-center align-items-center');
+        expect(div5Element).toHaveClass('d-flex flex-row bg-light');
+    })
+})
